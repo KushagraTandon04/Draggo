d9cc3e0e2ddf0de71dd7f11e7c9423681446a48e
Kushagra Tandon <142966158+KushagraTandon04@users.noreply.github.com>
2025-06-08 18:10:58 +0530

    Update vite.config.js

--- kanban-board-mern-main/frontend/vite.config.js
+++ kanban-board-mern-main/frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      outDir: 'public',  // 
+      outDir: 'dist',
       emptyOutDir: true,
     },
     plugins: [react()],
